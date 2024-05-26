fd4f51423f36f03b22a2694bda633480738b032e
Anna <annarosewain9@gmail.com>
2024-05-26 21:50:38 +0100

    Add images to Home.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "../styles/Header.css";
+import Logo from "../styles/imgs/icons/logo.svg";
 
 export default function Header() {
   return (
     <div className="Header">
       <nav className="nav">
         <a href="/" className="site-logo">
-          <img src=""></img>
+          <img src={Logo} alt="Rose Long Logo" className="site-logo" />
         </a>
       </nav>
       <h1>Rose Long</h1>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "../styles/Home.css";
 import Header from "../components/Header";
+import HeroImg from "../styles/imgs/permanent/hero-image.jpg";
+
+//navbar
+//side-bar with insta and vimeo: https://codesandbox.io/p/sandbox/fixed-sidebar-oenio?file=%2Fsrc%2Fstyles.css%3A15%2C25
+//make it responsive
+//on-hover image with png and button
 
 export default function Home() {
   return (
     <div className="Home">
       <Header />
+      <div className="hero-image">
+        <img
+          className="hero-img"
+          src={HeroImg}
+          alt="Woman doing child's pose on orange yoga matt holding hands in prayer above her head."
+        />
+      </div>
     </div>
   );
 }
