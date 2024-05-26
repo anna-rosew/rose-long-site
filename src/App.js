2374af3ec8dd3c924e6010a819d5525f4caedb9a
Anna <annarosewain9@gmail.com>
2024-05-26 20:00:26 +0100

    React Router DOM sorted

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Work from "./pages/Work";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import NotPage from "./pages/NotPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/work" element={<Work />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/NotPage.js
+++ src/pages/NotPage.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
 
-export default function NoPage() {
+export default function NotPage() {
   return (
-    <div className="NoPage">
+    <div className="NotPage">
       <Header />
       <h2>Error 404: Not Found</h2>
     </div>
