9dcaf9eebfc57302d63c08e5284ebc7334d7f6b3
Anna <annarosewain9@gmail.com>
2024-05-26 14:16:56 +0100

    NoPage set-up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/NoPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function NoPage() {
+  return (
+    <div className="NoPage">
+      <Header />
+      <h2>Error 404: Not Found</h2>
+    </div>
+  );
+}
