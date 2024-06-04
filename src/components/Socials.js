8d79fc0830ee8bc5a03dc1477b24990669f13462
Anna <annarosewain9@gmail.com>
2024-06-04 14:48:10 +0100

    Social Icons component created and styled

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "../styles/Header.css";
+import InstaIcon from "../styles/imgs/icons/insta_icon.svg";
+import VimeoIcon from "../styles/imgs/icons/vimeo_icon.svg";
 
 const Socials = () => {
   return (
     <div className="social-icons-container">
       <a
-        href="https://www.facebook.com"
+        href="https://www.instagram.com/lifechangesrose/?hl=en"
         target="_blank"
         rel="noopener noreferrer"
         className="social-icon"
       >
-        <img src="facebook-icon.png" alt="Facebook" />
+        <img src={InstaIcon} alt="Instagram" />
       </a>
       <a
-        href="https://www.twitter.com"
+        href="https://www.roselong-lifechanges.com/"
         target="_blank"
         rel="noopener noreferrer"
         className="social-icon"
       >
-        <img src="twitter-icon.png" alt="Twitter" />
+        <img src={VimeoIcon} alt="Vimeo" />
       </a>
     </div>
   );
