6ebfac8aa5ff36be2a38fdec7b8821e2ffed588a
Anna <annarosewain9@gmail.com>
2024-06-16 10:40:02 +0100

    Creating an image slider

--- src/pages/Retreats.js
+++ src/pages/Retreats.js
@@ -1,101 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
+import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import Retreat1 from "../styles/imgs/photos/retreat1.png";
-import Retreat2 from "../styles/imgs/photos/retreat2.png";
-import Retreat3 from "../styles/imgs/photos/retreat3.png";
-import Workshop1 from "../styles/imgs/photos/workshop1.jpeg";
-import Workshop2 from "../styles/imgs/photos/workshop2.jpeg";
-import Workshop3 from "../styles/imgs/photos/workshop3.jpeg";
+//import Retreat2 from "../styles/imgs/photos/retreat2.png";
+//import Retreat3 from "../styles/imgs/photos/retreat3.png";
 import Location from "../styles/imgs/icons/location.svg";
 import Calendar from "../styles/imgs/icons/calender.svg";
 import "../styles/Retreat.css";
-
-const events = [
-  {
-    type: "Retreat",
-    title: "My Heart is For The Whole",
-    location: "Patrick Brompton Hall, Yorkshire Dales, UK",
-    date: "1st - 7th May 2024",
-    description: "Immerse yourself in a weekend of yoga and breath work...",
-    images: [Retreat1, Retreat2, Retreat3],
-  },
-  {
-    type: "Workshop",
-    title: "Group Workshop",
-    location: "Somewhere, UK",
-    date: "10th - 12th June 2024",
-    description: "Join us for an engaging workshop...",
-    images: [Workshop1, Workshop2, Workshop3],
-  },
-];
+import "../styles/App.css";
 
 export default function Retreats() {
-  const [currentEventIndex, setCurrentEventIndex] = useState(0);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const currentEvent = events[currentEventIndex];
-  const totalEvents = events.length;
-  const totalImages = currentEvent.images.length;
-
-  const handleNextEvent = () => {
-    setCurrentEventIndex((prevIndex) => (prevIndex + 1) % totalEvents);
-    setCurrentImageIndex(0);
-  };
-
-  const handlePreviousEvent = () => {
-    setCurrentEventIndex(
-      (prevIndex) => (prevIndex - 1 + totalEvents) % totalEvents
-    );
-    setCurrentImageIndex(0);
-  };
-
-  const handleNextImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % totalImages);
-  };
-
-  const handlePreviousImage = () => {
-    setCurrentImageIndex(
-      (prevIndex) => (prevIndex - 1 + totalImages) % totalImages
-    );
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 100,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
     <div className="Retreats">
       <div className="container Retreat-page">
         <div className="grid grid-2-columns">
-          <div className="retreat-image-column">
-            <img
-              src={currentEvent.images[currentImageIndex]}
-              className="img-responsive img-hover"
-              alt={currentEvent.type}
-            />
-            <div className="image-navigation">
-              <button onClick={handlePreviousImage}>{"<"}</button>
-              <button onClick={handleNextImage}>{">"}</button>
-            </div>
+          <div className="slider-container">
+            <Slider {...settings}>
+              <div>
+                <img src={Retreat1} alt="Reatreat1"></img>
+              </div>
+              <div>2</div>
+              <div>3</div>
+            </Slider>
           </div>
           <div className="retreat-info-column">
-            <h3>{currentEvent.type}</h3>
-            <h1>{currentEvent.title}</h1>
+            <h3>Upcoming Retreat</h3>
+            <h1>My Heart is For The Whole</h1>
             <p>
               <img
                 src={Location}
                 alt="Location Icon"
                 className="details-icon"
               />{" "}
-              {currentEvent.location}
+              Patrick Brompton Hall, Yorkshire Dales, UK
             </p>
             <p>
-              <img src={Calendar} alt="Calendar Icon" /> {currentEvent.date}
+              <img
+                src={Calendar}
+                alt="Calendar Icon"
+                className="details-icon"
+              />{" "}
+              1st - 7th May 2024
             </p>
-            <p>{currentEvent.description}</p>
+            <p>
+              Immerse yourself in a weekend of yoga and breath work,
+              rediscovering wholeness within. Explore the interconnectedness of
+              health, holiness, and completeness, breaking free from patterns of
+              separation. Using the Chakras as a guide, journey inward to unlock
+              inner coherence and embrace all aspects of yourself.
+            </p>
             <Link to="/contact" className="get-in-touch">
-              <button className="get-in-touch">GET IN TOUCH</button>
+              GET IN TOUCH
             </Link>
-            <div className="event-navigation">
-              <button onClick={handlePreviousEvent}>{"<"}</button>
-              <button onClick={handleNextEvent}>{">"}</button>
-            </div>
           </div>
         </div>
       </div>
