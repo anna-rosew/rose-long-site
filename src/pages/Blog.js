e7c1495f954d0298608f04e71e5d6db28229ad93
Anna <annarosewain9@gmail.com>
2024-07-04 18:42:09 +0100

    Upload image and create blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,22 @@
 import React from "react";
+import "../styles/Blog.css";
+import mindOverMatter from ""
 
 export default function Blog() {
-  return <div className="Blog"></div>;
+  return (
+    <div className="Blog">
+      <div className="container">
+        <h1>Blog</h1>
+        <div className="blog-container-grid grid">
+          <div className="blog-block">
+            <img
+              src={mindOverMatter}
+              alt="Mind Over Matter Podcast: Episode 4"
+            ></img>
+          </div>
+          <div className="side-block grid grid-horizontal"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
