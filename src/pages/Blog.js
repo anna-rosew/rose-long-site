fcadf3ce4486c3b2d3b40775ca99ada62c030ae9
Anna <annarosewain9@gmail.com>
2024-07-04 18:45:28 +0100

    Blog image grid

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Blog.css";
-import mindOverMatter from ""
+import mindOverMatter from "../styles/imgs/photos/maze.png";
 
 export default function Blog() {
   return (
@@ -14,7 +14,20 @@ export default function Blog() {
               alt="Mind Over Matter Podcast: Episode 4"
             ></img>
           </div>
-          <div className="side-block grid grid-horizontal"></div>
+          <div className="side-block grid grid-horizontal">
+            <div className="blog-block">
+              <img
+                src={mindOverMatter}
+                alt="Mind Over Matter Podcast: Episode 4"
+              ></img>
+            </div>
+            <div className="blog-block">
+              <img
+                src={mindOverMatter}
+                alt="Mind Over Matter Podcast: Episode 4"
+              ></img>
+            </div>
+          </div>
         </div>
       </div>
     </div>
