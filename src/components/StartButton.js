6273fc1d43ebee7d739a33dbad7dc4c29434c600
Anna <annarosewain9@gmail.com>
2024-07-11 15:02:52 +0100

    Revert "Import VidStack for Vimeo player"
    This reverts commit 3b23f8694716c71fcb96974568744d13ed55707f.

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function StartButton() {
   return (
-    <Link to="/work">
+    <Link to="/approach">
       <button className="start-journey-button">Start Journey</button>
     </Link>
   );
