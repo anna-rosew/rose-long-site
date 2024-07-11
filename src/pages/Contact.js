3dc8eff42a5d01143d775904293503313c9cefeb
Anna <annarosewain9@gmail.com>
2024-07-11 13:32:34 +0100

    Newsletter element added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Contact.css";
 import ContactImg from "../styles/imgs/permanent/contact.png";
 import newsletterIcon from "../styles/imgs/icons/newsletter.svg";
@@ -8,49 +8,11 @@ import newsletterIcon from "../styles/imgs/icons/newsletter.svg";
 //Alert styling
 
 export default function Contact() {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Here you can handle the form submission, e.g., send the data to your server or a third-party service
-    console.log(formData);
-    alert("Form submitted!");
-    // Reset the form
-    setFormData({
-      name: "",
-      email: "",
-      message: "",
-    });
-  };
-
   return (
     <div className="contact">
       <div className="container">
         <div className="heading-container">
-          <h2>Get</h2>
-          <h1>In-Touch</h1>
-        </div>
-        <div className="call-to-action">
-          <p>
-            If you have any questions or would like to book a class, workshop or
-            retreat, please get in touch below. I usually get back to you within
-            48 hours.
-          </p>
-          <p>
-            You can also stay-in-touch by signing up for the monthly newsletter.
-          </p>
+          <h1>Contact</h1>
         </div>
 
         <div className="grid grid-contact">
@@ -63,49 +25,50 @@ export default function Contact() {
           </div>
           <div className="forms-container">
             <div className="contact-form-container">
-              <h2>Get-In Touch</h2>
-              <form onSubmit={handleSubmit} className="contact-form">
+              <h2>Get-In Touch</h2>{" "}
+              <p>
+                If you'd like to book a class, workshop or retreat or have any
+                questions, please don't hesitate to reach out. I will get back
+                to you within 2 working days. I look forward to hearing from
+                you!{" "}
+              </p>
+              <form className="contact-form">
                 <div className="form-group">
                   <label htmlFor="name">NAME</label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    required
-                  />
+                  <input type="text" id="name" name="name" required />
                 </div>
                 <div className="form-group">
                   <label htmlFor="email">Email</label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                  />
+                  <input type="email" id="email" name="email" required />
                 </div>
                 <div className="form-group">
                   <label htmlFor="message">Message</label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    required
-                  ></textarea>
+                  <textarea id="message" name="message" required></textarea>
                 </div>
                 <button type="submit" className="submit-btn">
                   SEND
                 </button>
               </form>
             </div>
-            <div className="stay-in-touch">
+            <div className="contact-form-container">
               <h2>Stay-in-Touch</h2>
-              <img src={newsletterIcon} alt="Newletter" />
-              <p></p>
+              <div className="stay-in-touch">
+                <img
+                  src={newsletterIcon}
+                  alt="Newletter"
+                  className="newletter-icon"
+                />
+                <div className="newsletter-subscribe">
+                  <p>
+                    If you’d like to stay up-to-date with classes, workshops and
+                    retreats, please sign-up to the monthly newsletter. I like
+                    to keep it short but sweet. You can unsubscribe at anytime.
+                  </p>
+                  <button type="submit" className="submit-btn">
+                    SIGN-UP
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
