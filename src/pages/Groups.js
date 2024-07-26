031321270f9e9f2dda0d70d9e45eb909c6d5e547
Anna <annarosewain9@gmail.com>
2024-07-26 18:44:11 +0100

    Changing overlay colour to white

--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -11,7 +11,7 @@ import MorningBreathe from "../styles/imgs/icons/morning-breath.svg";
 import EveningChill from "../styles/imgs/icons/evening-chill.svg";
 import Lunar from "../styles/imgs/icons/lunar.svg";
 import Kundalini from "../styles/imgs/icons/kundalini.svg";
-import OverlaySVG from "../styles/imgs/icons/sunshine.svg";
+import OverlaySVG from "../styles/imgs/icons/sunshine_white.svg";
 import Workshop from "../styles/imgs/photos/groups.png";
 import Location from "../styles/imgs/icons/location.svg";
 import Calender from "../styles/imgs/icons/calender.svg";
