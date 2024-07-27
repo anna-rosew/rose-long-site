6cb2ffce20b8830ebd973411e66685ba9a17f6c1
Anna <annarosewain9@gmail.com>
2024-07-27 16:57:24 +0100

    Format page 404

--- src/pages/NotPage.js
+++ src/pages/NotPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Header from "../components/Nav/Header";
+import { Link } from "react-router-dom";
+import "../styles/App.css";
+import sunshine from "../styles/imgs/icons/sunshine.svg";
 
 export default function NotPage() {
   return (
     <div className="NotPage">
-      <Header />
-      <h2>Error 404: Not Found</h2>
+      <div style={{ textAlign: "center", padding: "50px" }}>
+        <h1>404</h1>
+        <p>
+          Ah, I think we may be lost. The page you're looking for doesn't exist.
+        </p>
+        <img src={sunshine} alt="sunshine" />
+        <Link to="/home">
+          <button className="general-button">Go Back Home</button>
+        </Link>
+      </div>
     </div>
   );
 }
