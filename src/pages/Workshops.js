6010bc4b55d974140018b0824bac7f540536a2c1
Anna <annarosewain9@gmail.com>
2024-07-31 16:31:53 +0100

    Experimenting with slides

--- src/pages/Workshops.js
+++ src/pages/Workshops.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Slider from "react-slick";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -11,61 +9,25 @@ import Workshop2 from "../styles/imgs/photos/workshop2.png";
 import Workshop3 from "../styles/imgs/photos/workshop3.png";
 import Location from "../styles/imgs/icons/location.svg";
 import Calendar from "../styles/imgs/icons/calender.svg";
-import { NextArrow, PrevArrow } from "../components/CustomArrow";
 import "../styles/Retreat.css";
 import "../styles/App.css";
-
 import { Pagination, Navigation } from "swiper/modules";
 
 export default function Workshops() {
-  const innerSliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
-
   return (
     <div className="Workshops">
       <div className="container retreat-page">
         <h1>Workshops</h1>
         <Swiper
-          pagination={{
-            type: "fraction",
-          }}
+          pagination={{ type: "fraction" }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="mySwiper"
+          className="workshopSwiper"
         >
           <SwiperSlide>
             <div className="event-container">
               <div className="slider-container">
-                <Slider {...innerSliderSettings}>
-                  <div>
-                    <img
-                      src={Workshop1}
-                      alt="Workshop1"
-                      className="retreat-img"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={Workshop2}
-                      alt="Workshop2"
-                      className="retreat-img"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={Workshop3}
-                      alt="Workshop3"
-                      className="retreat-img"
-                    />
-                  </div>
-                </Slider>
+                <EmbeddedSwiper />
               </div>
               <div className="retreat-info-column">
                 <h3>Upcoming Workshop</h3>
@@ -96,7 +58,7 @@ export default function Workshops() {
                   Experience guided meditations, workshops, and discussions
                   designed to unlock your potential and elevate your journey.
                 </p>
-                <Link to="/contact" className="nav-buttons">
+                <Link to="/contact" className="nav-buttons get-in-touch">
                   GET IN TOUCH
                 </Link>
               </div>
@@ -105,29 +67,7 @@ export default function Workshops() {
           <SwiperSlide>
             <div className="event-container">
               <div className="slider-container">
-                <Slider {...innerSliderSettings}>
-                  <div>
-                    <img
-                      src={Workshop1}
-                      alt="Workshop1"
-                      className="retreat-img"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={Workshop2}
-                      alt="Workshop2"
-                      className="retreat-img"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      src={Workshop3}
-                      alt="Workshop3"
-                      className="retreat-img"
-                    />
-                  </div>
-                </Slider>
+                <EmbeddedSwiper />
               </div>
               <div className="retreat-info-column">
                 <h3>Upcoming Workshop</h3>
@@ -158,7 +98,7 @@ export default function Workshops() {
                   Experience guided meditations, workshops, and discussions
                   designed to unlock your potential and elevate your journey.
                 </p>
-                <Link to="/contact" className="nav-buttons">
+                <Link to="/contact" className="nav-buttons get-in-touch">
                   GET IN TOUCH
                 </Link>
               </div>
@@ -167,5 +107,26 @@ export default function Workshops() {
         </Swiper>
       </div>
     </div>
+  );
+}
+
+function EmbeddedSwiper() {
+  return (
+    <Swiper
+      navigation={true} //
+      pagination={{ clickable: true }}
+      modules={[Pagination]}
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <img src={Workshop1} alt="Workshop1" className="retreat-img" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Workshop2} alt="Workshop2" className="retreat-img" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Workshop3} alt="Workshop3" className="retreat-img" />
+      </SwiperSlide>
+    </Swiper>
   );
 }
