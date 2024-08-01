faf68e15582dfcf7a78cf61c4ff3fe43a666a749
Anna <annarosewain9@gmail.com>
2024-08-01 12:19:59 +0100

    Blog page adjust

--- src/pages/BlogPosts/BlogArticleTemp.js
+++ src/pages/BlogPosts/BlogArticleTemp.js
@@ -1,8 +1,11 @@
-import Recat from "react;
+import React from "react";
 import ".../styles/App.css";
+import ".../styles/Article.css";
 
-export default function BlogArticleTemp(){
-return (<div className="BlogArticle container">
-<h1></h1>
-</div>)
+export default function BlogArticleTemp() {
+  return (
+    <div className="BlogArticle container">
+      <h1></h1>
+    </div>
+  );
 }
