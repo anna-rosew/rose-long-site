293f05cd86455378318b147a774eb84650c57fe5
Anna <annarosewain9@gmail.com>
2024-08-02 11:32:18 +0100

    Changing image grid image grid

--- src/pages/InPerson.js
+++ src/pages/InPerson.js
@@ -1,69 +1,75 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import Location from "../styles/imgs/icons/location.svg";
+import Calendar from "../styles/imgs/icons/calender.svg";
+import Time from "../styles/imgs/icons/time.svg";
 import "../styles/InPerson.css";
-import { Pagination, Navigation } from "swiper/modules";
-import EventSlider from "../components/InPersonSlider";
-import prevArrow from "../styles/imgs/icons/arrow-prev.svg";
-import nextArrow from "../styles/imgs/icons/arrow-next.svg";
+import { Link } from "react-router-dom";
 
-export default function InPerson() {
-  const events = [
-    {
-      images: [require("../styles/imgs/photos/workshop1.png")],
-      title: "Moving Beyond Separation: The Power of The Breath.",
-      location: "Home Studio, Liverpool. UK",
-      date: "15th - 17th August 2024",
-      time: "10:00 AM - 04:00 PM",
-      description:
-        "Most of us spend most of our time in thoughts, analysing, judging, assessing, reflecting, projecting, and so it goes on.  Thoughts separate this from that, me from you, inner from outer.  Living in thoughts it’s easy to miss the unified experience of life, and all that brings.  [strong]The breath brings us back  to wholeness. [/strong] As we breathe consciously the connecting flow of the breath becomes a living teacher.  The barriers dissolve, the walls crumble, separation is bridged.  [strong]We experience ourselves as a unified whole, connected to the unified whole. [/strong]",
-      description2:
-        "This event combines [strong]movement and breathwork,[/strong] opening you to the unifying capacity of the breath.  [strong] Suitable for those of you who’ve worked with me before and have some experience of Kundalini breathwork.[/strong] ",
-      maxCapacity: "Max 7 participants",
-      price: "£25",
-    },
-    {
-      images: [require("../styles/imgs/photos/workshop1.png")],
-      title: "Return to the Heart",
-      location: "Home Studio, Liverpool. UK",
-      date: "15th - 17th August 2024",
-      time: "10:00 AM - 04:00 PM",
-      description: "Join us for a transformative event...",
-      description2: "hello",
-      maxCapacity: "50 participants",
-      price: "£25",
-    },
-  ];
+// Utility function to replace markers with HTML tags
+const formatDescription = (text) => {
+  // Replace [strong]...[/strong] with <strong>...</strong>
+  return text.replace(/\[strong\](.*?)\[\/strong\]/g, "<strong>$1</strong>");
+};
 
+export default function InPersonSlider({ eventInfo }) {
   return (
-    <div className="Events">
-      <div className="container in-person-page">
-        <h1>In-Person Sessions</h1>
-        <h3 className="subtitle">YOGA | COMMUNITY</h3>
-        <Swiper
-          pagination={{ clickable: true }}
-          navigation={{
-            nextEl: ".event-swiper-button-next",
-            prevEl: ".event-swiper-button-prev",
-          }}
-          modules={[Pagination, Navigation]}
-          className="event-swiper"
-        >
-          {events.map((event, index) => (
-            <SwiperSlide key={index}>
-              <EventSlider eventInfo={event} />
-            </SwiperSlide>
+    <div className="event-slide">
+      <div className="event-container">
+        <div className="image-gallery">
+          {eventInfo.images.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Event ${index + 1}`}
+              className={`event-img ${
+                index === 0 ? "large-image" : "small-image"
+              }`}
+            />
           ))}
-          {/* Custom Navigation Buttons */}
-          <div className="event-swiper-button-prev">
-            <img src={prevArrow} alt="prev arrow" className="nav-arrow" />
-          </div>
-          <div className="event-swiper-button-next">
-            <img src={nextArrow} alt="next arrow" className="nav-arrow" />
-          </div>
-        </Swiper>
+        </div>
+        <div className="event-info-column">
+          <h3>Upcoming Event</h3>
+          <h1>{eventInfo.title}</h1>
+          <p>
+            <span className="details-span">
+              <img
+                src={Location}
+                alt="Location Icon"
+                className="details-icon"
+              />
+              {eventInfo.location}
+            </span>
+          </p>
+          <p>
+            <span className="details-span">
+              <img
+                src={Calendar}
+                alt="Calendar Icon"
+                className="details-icon"
+              />
+              {eventInfo.date}
+            </span>
+          </p>
+          <p>
+            <span className="details-span">
+              <img src={Time} alt="Time Icon" className="details-icon" />
+              {eventInfo.time}
+            </span>
+          </p>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: formatDescription(eventInfo.description),
+            }}
+          />
+          <p>{eventInfo.description2}</p>
+          <p>AVAILABILITY: {eventInfo.maxCapacity}</p>
+          <p>
+            PRICE: <strong>{eventInfo.price}</strong>
+          </p>
+          <Link to="/contact">
+            <button className="get-in-touch">BOOK NOW</button>{" "}
+          </Link>
+        </div>
       </div>
     </div>
   );
