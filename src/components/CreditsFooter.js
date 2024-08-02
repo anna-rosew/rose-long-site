fd6656a087d17bbba6719e91dfcdbf4e0b12fd1d
Anna <annarosewain9@gmail.com>
2024-08-02 18:16:39 +0100

    Updating footer and retreats page

--- src/components/CreditsFooter.js
+++ src/components/CreditsFooter.js
@@ -1,17 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/CreditsFooter.css";
 import Logo from "../styles/imgs/icons/logo_white.svg";
+import InstaIcon from "../styles/imgs/icons/insta-footer.svg";
+import VimeoIcon from "../styles/imgs/icons/footer-vimeo.svg";
 
 export default function CreditsFooter() {
   return (
     <div className="CreditsFooter">
-      <div className="logo">
-        <div className="logo">
-          <a href="/" className="site-logo">
-            <img src={Logo} alt="Rose Long Logo" className="site-logo" />
+      <div className="logo-footer">
+        <a href="/">
+          <img src={Logo} alt="Rose Long Logo" className="site-logo-footer" />
+        </a>
+        <div className="socials">
+          <a
+            href="https://www.instagram.com/lifechangesrose/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-footer"
+          >
+            <img src={InstaIcon} alt="Instagram" />
+          </a>
+          <a
+            href="https://vimeo.com/roselong"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-footer"
+          >
+            <img src={VimeoIcon} alt="Vimeo" />
           </a>
         </div>
       </div>
+
       <div className="credits">
         <p>
           Website built by{" "}
@@ -31,7 +51,44 @@ export default function CreditsFooter() {
           </a>
         </p>
       </div>
-      <div className="all-pages"></div>
+      <div className="all-pages">
+        <div className="list">
+          <h3>services</h3>
+          <ul>
+            <li>
+              <Link to="/groups">Groups Overview</Link>
+            </li>
+            <li>
+              <Link to="/onetoone">One-To-Ones</Link>
+            </li>
+            <li>
+              <Link to="/groups">Online Groups</Link>
+            </li>
+            <li>
+              <Link to="/inperson">In-Person Sessions</Link>
+            </li>
+            <li>
+              <Link to="/retreats">Retreats</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="list">
+          <h3>about</h3>
+          <ul>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="list">
+          <h3>resources</h3>
+          <ul>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Retreats.js
+++ src/pages/Retreats.js
@@ -1,76 +1,36 @@
 import React from "react";
-import Slider from "react-slick";
+import sunshine from "../styles/imgs/icons/sunshine.svg";
 import { Link } from "react-router-dom";
-import Retreat1 from "../styles/imgs/photos/retreat1.png";
-import Retreat2 from "../styles/imgs/photos/retreat2.png";
-import Retreat3 from "../styles/imgs/photos/retreat3.png";
-import Location from "../styles/imgs/icons/location.svg";
-import Calendar from "../styles/imgs/icons/calender.svg";
-import { NextArrow, PrevArrow } from "../components/CustomArrow";
+
 import "../styles/Retreat.css";
 import "../styles/App.css";
 
 export default function Retreats() {
-  const innerSliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
-
   return (
     <div className="Retreats">
       <div className="container retreat-page">
         <h1>Retreats</h1>
-        <div className="event-container">
-          <div className="slider-container">
-            <Slider {...innerSliderSettings}>
-              <div>
-                <img src={Retreat1} alt="Retreat1" className="retreat-img" />
-              </div>
-              <div>
-                <img src={Retreat2} alt="Retreat2" className="retreat-img" />
-              </div>
-              <div>
-                <img src={Retreat3} alt="Retreat3" className="retreat-img" />
-              </div>
-            </Slider>
-          </div>
-          <div className="retreat-info-column">
-            <h3>Upcoming Retreat</h3>
-            <h1>My Heart is For The Whole</h1>
+        <div className="membership-text">
+          <img src={sunshine} alt="sunshine" />
+          <p>
+            There are currently no retreats planned but be sure to{" "}
+            <Link to="/contact" className="word-link">
+              <strong>sign-up to the newsletter</strong>
+            </Link>{" "}
+            to stay posted.
+          </p>
+          <div className="current-deals">
+            <h3>Current Monthly membership deals</h3>
             <p>
-              <span className="details-span">
-                <img
-                  src={Location}
-                  alt="Location Icon"
-                  className="details-icon"
-                />
-                Patrick Brompton Hall, Yorkshire Dales, UK
-              </span>
+              <strong>Monthly Bundles:</strong> Morning Breath, Deep Chill,
+              Kundalini Magic and Lunar Cycles.
             </p>
             <p>
-              <span className="details-span">
-                <img
-                  src={Calendar}
-                  alt="Calendar Icon"
-                  className="details-icon"
-                />
-                1st - 7th May 2024
-              </span>
+              <strong>Monthly Membership:</strong> Access all online classes
+              live and access to video playbacks to watch again at any time.
             </p>
-            <p>
-              Immerse yourself in a weekend of yoga and breath work,
-              rediscovering wholeness within. Explore the interconnectedness of
-              health, holiness, and completeness, breaking free from patterns of
-              separation. Using the Chakras as a guide, journey inward to unlock
-              inner coherence and embrace all aspects of yourself.
-            </p>
-            <Link to="/contact" className="get-in-touch">
-              GET IN TOUCH
+            <Link to="/contact">
+              <button className="general-button">Book</button>
             </Link>
           </div>
         </div>
