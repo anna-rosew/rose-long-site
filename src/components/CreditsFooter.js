e7b9d8e1bbbeb1718c6d3d032287249471985ca9
Anna <annarosewain9@gmail.com>
2024-08-02 17:49:28 +0100

    Footer logo

--- src/components/CreditsFooter.js
+++ src/components/CreditsFooter.js
@@ -1,26 +1,37 @@
 import React from "react";
 import "../styles/CreditsFooter.css";
+import Logo from "../styles/imgs/icons/logo_white.svg";
 
 export default function CreditsFooter() {
   return (
     <div className="CreditsFooter">
-      <p>
-        Website built by{" "}
-        <a
-          href="https://www.sacredintuitiveart.com/about"
-          alt="Anna-Rose Wain | Front-end Developer"
-        >
-          {" "}
-          Anna-Rose Wain{" "}
-        </a>{" "}
-        | Artwork by{" "}
-        <a
-          href="https://www.sacredintuitiveart.com/about"
-          alt="Sacred Intuitive Art"
-        >
-          Lynn Hanford Day - Sacred Intuitive Art
-        </a>
-      </p>
+      <div className="logo">
+        <div className="logo">
+          <a href="/" className="site-logo">
+            <img src={Logo} alt="Rose Long Logo" className="site-logo" />
+          </a>
+        </div>
+      </div>
+      <div className="credits">
+        <p>
+          Website built by{" "}
+          <a
+            href="https://www.sacredintuitiveart.com/about"
+            alt="Anna-Rose Wain | Front-end Developer"
+          >
+            {" "}
+            Anna-Rose Wain{" "}
+          </a>{" "}
+          | Artwork by{" "}
+          <a
+            href="https://www.sacredintuitiveart.com/about"
+            alt="Sacred Intuitive Art"
+          >
+            Lynn Hanford Day - Sacred Intuitive Art
+          </a>
+        </p>
+      </div>
+      <div className="all-pages"></div>
     </div>
   );
 }
