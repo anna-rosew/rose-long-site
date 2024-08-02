35c6199e31b794db0e546b0097d7a066dc0ee317
Anna <annarosewain9@gmail.com>
2024-08-02 16:10:12 +0100

    Add bullet to contents item on hover

--- src/pages/BlogPosts/BlogArticleTemp.js
+++ src/pages/BlogPosts/BlogArticleTemp.js
@@ -6,6 +6,9 @@ import FoodImg from "../../styles/imgs/blog/food_blog.png";
 import arrow from "../../styles/imgs/icons/arrow-next.svg";
 
 export default function BlogArticleTemp() {
+  const onClick = () => {
+    window.location.href = "#self-awareness";
+  };
   return (
     <div className="container">
       <div className="blog-article">
@@ -49,12 +52,11 @@ export default function BlogArticleTemp() {
           <div className="contents">
             <h3>CONTENTS:</h3>
             <ul>
-              <li className="contents-link">
-                <span>
-                  <button>Introduction</button>
-                  <img src={arrow} alt="arrow" />
-                </span>
+              <li className="contents-link" onClick={onClick}>
+                Introduction
+                <img src={arrow} alt="arrow" className="arrow-nav" />
               </li>
+              {/* Add more list items as needed */}
             </ul>
           </div>
           <div className="article-intro-text">
@@ -97,10 +99,61 @@ export default function BlogArticleTemp() {
             steps can I take today?’.
           </p>
         </div>
-        <div className="article-section">
+        <div className="article-section" id="self-awareness">
           <h2 className="article-subheading">
             The Challenge of Deepening your Self Awareness
           </h2>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32. The standard chunk of
+            Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+            Malorum" by Cicero are also reproduced in their exact original form,
+            accompanied by English versions from the 1914 translation by H.
+            Rackham.
+          </p>
+        </div>
+        <div className="article-section" id="inside-out">
+          <h2 className="article-subheading">
+            Moving from Outside In to Inside Out
+          </h2>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32. The standard chunk of
+            Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+            Malorum" by Cicero are also reproduced in their exact original form,
+            accompanied by English versions from the 1914 translation by H.
+            Rackham.
+          </p>
+        </div>
+        <div className="article-highlight">
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia.
+          </p>
         </div>
       </div>
     </div>
