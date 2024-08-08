66862ad2ec5995fca9999e1911429abbd91fc87a
Anna <annarosewain9@gmail.com>
2024-08-08 17:43:41 +0100

    Editing spacing in navbar and footer

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,32 +1,37 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Burger from "./Burger";
-import "../../styles/NavBar.css";
 
 const Nav = styled.nav`
   width: 100%;
   height: 150px;
   padding: 0 20px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
+  position: relative;
+
+  .logo {
+    position: absolute;
+    top: 0;
+    left: 20px;
+  }
 
   .site-logo {
     height: 150px;
-    color: black;
     margin-top: 30px;
-    margin-left: 30px;
   }
 
   @media (max-width: 768px) {
-    padding: 0px;
-    width: 100%;
     height: 100px;
-    justify-content: center;
+    justify-content: space-between;
+
+    .logo {
+      position: static;
+    }
 
     .site-logo {
       height: 60px;
-      text-align: center;
       margin-left: 0;
       margin-top: 10px;
     }
